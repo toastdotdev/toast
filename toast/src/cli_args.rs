a76af589d37390352706034220da8b548cb94507
Dan Thompson <danthompson41@gmail.com>
2020-10-06 21:58:09 -0400

    Updated cli arguments
    Added a description to incremental, and updated descriptions for input and output directories to reflect implementation

--- toast/src/cli_args.rs
+++ toast/src/cli_args.rs
@@ -19,17 +19,19 @@ fn abspath(input_dir: &str) -> Result<PathBuf> {
 #[derive(Debug, StructOpt)]
 #[structopt(name = "toast", about = "The best place to stack your JAM")]
 pub enum Toast {
+    /// Incrementally build your input directory
+    #[structopt(name = "incremental")]
     Incremental {
         /// Activate debug mode
         // short and long flags (-d, --debug) will be deduced from the field's name
         #[structopt(short, long)]
         debug: bool,
 
-        /// Input file
+        /// Input directory 
         #[structopt(parse(try_from_str = abspath))]
         input_dir: PathBuf,
 
-        /// Output file, stdout if not present
+        /// Output directory, "./public" if not present
         #[structopt(parse(from_os_str))]
         output_dir: Option<PathBuf>,
     },
