7bc78540c4fa8db88d592f1bbba5195e0ef72f47
Christopher Biscardi <chris@christopherbiscardi.com>
2020-10-11 16:08:12 -0700

    interleave

--- toast/src/incremental.rs
+++ toast/src/incremental.rs
@@ -132,8 +132,12 @@ pub async fn incremental_compile(opts: IncrementalOpts<'_>) -> Result<()> {
         .iter()
         .map(|(_, output_file)| output_file.dest.clone())
         .collect::<Vec<String>>();
-    let _data_from_user =
-        source_data(&project_root_dir.join("toast.js"), npm_bin_dir.clone()).await;
+    let _data_from_user = source_data(
+        &project_root_dir.join("toast.js"),
+        npm_bin_dir.clone(),
+        create_pages_pb.clone(),
+    )
+    .await;
 
     let _maybe_gone = server.cancel();
     let _result = fs::remove_file("/var/tmp/toaster.sock");
@@ -265,6 +269,7 @@ pub async fn incremental_compile(opts: IncrementalOpts<'_>) -> Result<()> {
         output_dir.into_os_string().into_string().unwrap(),
         list,
         npm_bin_dir,
+        render_pb.clone(),
     )?;
     render_pb.abandon_with_message("html rendered");
 
--- toast/src/toast/node.rs
+++ toast/src/toast/node.rs
@@ -1,7 +1,11 @@
 use color_eyre::eyre::{eyre, Result};
-use std::io::Write;
-use std::path::PathBuf;
-use std::process::Command;
+use duct::cmd;
+use indicatif::ProgressBar;
+use std::{
+    io::{prelude::*, BufReader},
+    path::PathBuf,
+    sync::Arc,
+};
 use tracing::instrument;
 
 #[instrument]
@@ -10,56 +14,104 @@ pub fn render_to_html(
     output_dir: String,
     filepaths: Vec<String>,
     npm_bin_dir: PathBuf,
+    active_pb: Arc<ProgressBar>,
 ) -> Result<()> {
     let bin = npm_bin_dir.join("toast-render");
-    let mut cmd = Command::new("node");
     let bin_str = bin
         .to_str()
         .ok_or_else(|| eyre!("failed to make npm bin into str"))?;
-    cmd.args(&[
-        // "--no-warnings",
-        "--loader",
-        "toast/src/loader.mjs",
-        bin_str,
-        &dir_of_input_files,
-        &output_dir,
-    ]);
-    for arg in filepaths {
-        cmd.arg(arg);
+    let mut args: Vec<String> = vec![
+        "--loader".to_owned(),
+        "toast/src/loader.mjs".to_owned(),
+        bin_str.to_owned(),
+        dir_of_input_files,
+        output_dir,
+    ];
+    args.extend(filepaths.iter().cloned());
+    let output = cmd("node", args).stderr_to_stdout();
+    if let Ok(reader) = output.reader() {
+        let lines = BufReader::new(&reader).lines();
+        for (i, line) in lines.filter_map(|m| m.ok()).enumerate() {
+            // this magic number pulls off the warning
+            if i > 1 {
+                // if the progress bars are hidden, so is the
+                // output from the pb.println function
+                // so we use the println macro instead
+                if active_pb.is_hidden() {
+                    println!("{}", line)
+                } else {
+                    active_pb.println(line);
+                }
+            }
+        }
+        if let Ok(Some(output_status)) = &reader.try_wait() {
+            if output_status.status.success() {
+                return Ok(());
+            } else if let Some(code) = output_status.status.code() {
+                return Err(eyre!("renderToHtml node process exited with code {}", code));
+            }
+        }
+    } else {
+        return Err(eyre!("renderToHtml node process didn't start"));
     }
-    let output = cmd.output()?;
-    let _ = std::io::stdout().write_all(&output.stdout);
-    let _ = std::io::stderr().write_all(&output.stderr);
     Ok(())
 }
 
 #[instrument]
-pub async fn source_data(toast_js_file: &PathBuf, npm_bin_dir: PathBuf) -> Result<()> {
+pub async fn source_data(
+    toast_js_file: &PathBuf,
+    npm_bin_dir: PathBuf,
+    active_pb: Arc<ProgressBar>,
+) -> Result<()> {
     // not a guarantee that toast.js will exist when node
     // goes to look for it: just a sanity check to not
     // execute Command if we don't need to
     if toast_js_file.exists() {
         let bin = npm_bin_dir.join("toast-source-data");
-        let mut cmd = Command::new("node");
         let bin_str = bin
             .to_str()
             .ok_or_else(|| eyre!("failed to make npm bin into str"))?;
-        cmd.args(&[
-            // "--no-warnings",
+        let output = cmd!(
+            "node",
             "--loader",
             "toast/src/loader.mjs",
             bin_str,
             "/var/tmp/toaster.sock",
             &toast_js_file
                 .to_str()
-                .ok_or_else(|| eyre!("failed to make toast_js_file into str"))?,
-        ]);
-        let output = cmd.output()?;
-        // TODO: move stdout/stderr around so it's not just dumping to console
-        let _ = std::io::stdout().write_all(&output.stdout);
-        let _ = std::io::stderr().write_all(&output.stderr);
+                .ok_or_else(|| eyre!("failed to make toast_js_file into str"))?
+        )
+        .stderr_to_stdout();
+
+        if let Ok(reader) = output.reader() {
+            let lines = BufReader::new(&reader).lines();
+            for (i, line) in lines.filter_map(|m| m.ok()).enumerate() {
+                // this magic number pulls off the warning
+                if i > 1 {
+                    // if the progress bars are hidden, so is the
+                    // output from the pb.println function
+                    // so we use the println macro instead
+                    if active_pb.is_hidden() {
+                        println!("{}", line)
+                    } else {
+                        active_pb.println(line);
+                    }
+                }
+            }
+            if let Ok(Some(output_status)) = &reader.try_wait() {
+                if output_status.status.success() {
+                    return Ok(());
+                } else if let Some(code) = output_status.status.code() {
+                    return Err(eyre!("sourceData node process exited with code {}", code));
+                }
+            }
+        } else {
+            return Err(eyre!("sourceData node process didn't start"));
+        }
         Ok(())
     } else {
+        // toast file doesn't exist
+        // skip running sourceData
         Ok(())
     }
 }
