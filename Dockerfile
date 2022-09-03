1dded9ecb9b9470b25f21075e97c4ca0936a282b
Christopher Biscardi <chris@christopherbiscardi.com>
2022-09-03 10:34:10 -0700

    remove old toast

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM rust
-RUN rustup toolchain install nightly
-COPY . /opt/app
-WORKDIR /opt/app
-RUN make build-production
--- toast-node-wrapper/src/module-aliases.mjs
+++
@@ -1,8 +0,0 @@
-import moduleAlias from "module-alias";
-
-moduleAlias.addAliases({
-  react: "preact/compat",
-  "react-dom": "preact/compat",
-  "react/jsx-runtime": "preact/jsx-runtime",
-  //   'create-react-class': path.resolve(__dirname, './create-preact-class')
-});
